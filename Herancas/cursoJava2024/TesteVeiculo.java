1c32a09eef8fef98f052b2072d7bc3c9fd285ca6
brunapinhotech <brunapinho.tech@gmail.com>
2024-10-01 21:35:15 -0300

    Heranças

--- Herancas/cursoJava2024/TesteVeiculo.java
+++ Herancas/cursoJava2024/TesteVeiculo.java
@@ -3,9 +3,21 @@ package cursoJava2024;
 public class TesteVeiculo {
 	public static void main(String[] args) {
 		Carro carro = new Carro("Toyota", "Corrola", 2004, 4);
+		Carro car = new Carro("Fiat", "Uno", 1998, 2);
+		
+		Moto moto = new Moto("Honda", "CB500", 2019, true);
+		Moto motu = new Moto("BMW", "1200", 2020, false);
 
 		System.out.println("Detalhes do Carro: ");
 		carro.exibirDetalhes();
 		
+		System.out.println("\nDetalhes da Moto: ");
+		moto.exibirDetalhes();
+		
+		System.out.println("\nDetalhes do Carro: ");
+		car.exibirDetalhes();
+		
+		System.out.println("\nDetalhes da Moto: ");
+		motu.exibirDetalhes();
 	}
 }
